975bc525ea3d29172f59961921742c1bf6d09990
Sander Peguero <95009440+SanderPeguero@users.noreply.github.com>
2022-12-24 02:11:21 -0400

    Client Changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,7 +4,7 @@ import binance from "./binance.png";
 import card from "./card.png";
 import coinbase from "./coinbase.png";
 import dropbox from "./dropbox.png";
-import logo from "./BCZLogo.png";
+import logo from "./bcz-media-gold.png";
 import quotes from "./quotes.svg";
 import robot from "./robot.png";
 import send from "./Send.svg";
